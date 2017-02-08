d937170aac2e238a9ffa019da19b9a26db8e988d
3010a14 <3010a14@guanabana.ece.uprm.edu>
2017-02-08 16:58:11 -0400

    Change of name, dimensions, position and some spaces to push.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,11 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Kristalys Ruiz");
+		JFrame myFrame = new JFrame(" Kristalys Ruiz ");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 400);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		
 		
 	}
 }
