85c5a48360a2df79e10be8a5a18452d28c19b685
kristalys47 <kristalys.cba4@yahoo.com>
2017-02-10 02:07:51 -0400

    Bandera con estrella fija

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,8 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame(" Kristalys Ruiz ");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(850, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setLocation(500, 200);
+		myFrame.setSize(250,200);
 		MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
